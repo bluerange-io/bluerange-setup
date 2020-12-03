17a99fadac261840529822aafa3e17afd427a2db
Thomas Beckmann <thomas.beckmann@mway.io>
2020-12-03 09:26:07 +0100

    IOT-4811: update of script for compatibility with Ubuntu which is using dash instead of bash as a sh replacement shell

--- docker/bluerange-compose.sh
+++ docker/bluerange-compose.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 set -e
+cd $(dirname ${BASH_SOURCE:-$0})
 
 DOCKER_COMPOSE="docker-compose -p bluerange -f docker-compose.yml -f docker-compose.elasticsearch.yml -f docker-compose.mender.yml"
 
-source ./server.env
+. ./server.env
 export HOST
 
-if [[ $# -eq 0 ]] ; then
+if [ $# -eq 0 ] ; then
   echo "$" $DOCKER_COMPOSE up -d
   $DOCKER_COMPOSE up -d
   echo ""
